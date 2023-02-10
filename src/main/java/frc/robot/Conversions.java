2eea823d411f54e49e156ebd0c28bf7b5143c4e8
Team5235 <93394611+Team5235@users.noreply.github.com>
2023-02-10 12:48:01 -0500

    first commit

---
+++ src/main/java/frc/robot/Conversions.java
@@ -0,0 +1,21 @@
+package frc.robot;
+
+public class Conversions {
+
+    public static double degreesToFalcon(double d, double anglegearratio) {
+        return 0;
+    }
+
+    public static double falconToDegrees(double selectedSensorPosition, double anglegearratio) {
+        return 0;
+    }
+
+    public static double falconToMPS(double selectedSensorVelocity, double wheelcircumference, double drivegearratio) {
+        return 0;
+    }
+
+    public static double MPSToFalcon(double speedMetersPerSecond, double wheelcircumference, double drivegearratio) {
+        return 0;
+    }
+
+}
