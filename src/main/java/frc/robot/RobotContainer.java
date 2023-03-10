4fcedc0f681a760aa7c63b9bc87c02b3df0682a1
Jestin VanScoyoc <31118852+JediScoy@users.noreply.github.com>
2023-03-09 22:36:57 -0500

    Not working yet. Don't use!

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,6 +6,10 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+
+
 
 import frc.robot.autos.*;
 import frc.robot.commands.*;
@@ -68,6 +72,20 @@ public class RobotContainer {
      */
     public Command getAutonomousCommand() {
         // An ExampleCommand will run in autonomous
-        return new exampleAuto(s_Swerve);
+        
+        short polarity = 1;
+        double power = .4;
+        double seconds = 3;
+        final float input = (float) (polarity * power);
+        var driveCommand = new TeleopSwerve(
+                s_Swerve,
+                () -> input,
+                () -> 0,
+                () -> 0,
+                () -> robotCentric.getAsBoolean(),
+                () -> false);
+
+        return new ParallelDeadlineGroup(new WaitCommand(seconds), driveCommand);
+        
     }
 }
